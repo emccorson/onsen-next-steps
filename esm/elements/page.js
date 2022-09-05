9240b73b29165bfd89be2a179978d229f7e9ddd1
Eric Corson <public@giraffedev.com>
2022-09-05 08:16:26 +0100

    feat: add page template and modifier

--- esm/elements/page.js
+++ esm/elements/page.js
@@ -1,7 +1,19 @@
 import { compose } from '../util.js';
+import { withTemplate } from '../mixins/template.js';
+import { withModifier } from '../mixins/modifier.js';
+
+const template = document.createElement('template');
+template.innerHTML = `
+  <slot name="toolbar"></slot>
+  <div part="background"></div>
+  <div part="content">
+    <slot></slot>
+  </div>
+`;
 
 const page = compose(
-
+  withTemplate(template),
+  withModifier
 );
 
 export default page;
