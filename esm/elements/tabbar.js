635a88dfae198637c75d1198fb80683254331a2a
Eric Corson <public@giraffedev.com>
2022-09-05 08:53:34 +0100

    feat: add tabbar template and modifier

--- esm/elements/tabbar.js
+++ esm/elements/tabbar.js
@@ -1,7 +1,19 @@
 import { compose } from '../util.js';
+import { withTemplate } from '../mixins/template.js';
+import { withModifier } from '../mixins/modifier.js';
+
+const template = document.createElement('template');
+template.innerHTML = `
+  <div part="content">
+  </div>
+  <div>
+    <slot></slot>
+  </div>
+`;
 
 const tabbar = compose(
-
+  withTemplate(template),
+  withModifier
 );
 
 export default tabbar;
