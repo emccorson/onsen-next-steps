b2b9d285e1fe0296fb8fae421864e0bc9acf64e9
Eric Corson <public@giraffedev.com>
2022-09-05 09:42:39 +0100

    feat: add tabbar page loading

--- esm/elements/tab.js
+++ esm/elements/tab.js
@@ -1,8 +1,10 @@
 import { compose } from '../util.js';
 import { withTemplate } from '../mixins/template.js';
 import { withModifier } from '../mixins/modifier.js';
-import { withStringProperty } from '../mixins/property.js';
+import { withStringProperty, withBooleanProperty } from '../mixins/property.js';
 import { withEffect } from '../mixins/effect.js';
+
+const PAGE = 'page';
 
 const template = document.createElement('template');
 template.innerHTML = `
@@ -13,7 +15,6 @@ template.innerHTML = `
 `;
 
 const LABEL = 'label';
-
 const updateLabel = withEffect(
   LABEL,
   function (name, last, current) {
@@ -23,11 +24,45 @@ const updateLabel = withEffect(
   }
 );
 
+const ACTIVE = 'active';
+const updateActive = withEffect(
+  ACTIVE,
+  function (name, last, current) {
+    if (name === ACTIVE && this.active && (last === null || last === undefined)) {
+      this.dispatchEvent(new CustomEvent(`_${ACTIVE}`, {bubbles: true}));
+    }
+  }
+);
+
+const handleClick = function () {
+  this.active = true;
+};
+const activeOnClick = Base => class extends Base {
+  constructor() {
+    super();
+    this._activeOnClick = handleClick.bind(this);
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.addEventListener('click', this._activeOnClick);
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    this.removeEventListener('click', this._activeOnClick);
+  }
+};
+
 const tab = compose(
   withTemplate(template),
   withModifier,
+  withStringProperty(PAGE),
   withStringProperty(LABEL),
-  updateLabel
+  updateLabel,
+  withBooleanProperty(ACTIVE),
+  updateActive,
+  activeOnClick
 );
 
-export default tab;
+export { tab as default, ACTIVE, PAGE };
--- esm/elements/tabbar.js
+++ esm/elements/tabbar.js
@@ -1,19 +1,70 @@
 import { compose } from '../util.js';
 import { withTemplate } from '../mixins/template.js';
 import { withModifier } from '../mixins/modifier.js';
+import { ACTIVE, PAGE } from './tab.js';
 
 const template = document.createElement('template');
 template.innerHTML = `
-  <div part="content">
+  <div part="content" id="content">
   </div>
   <div>
     <slot></slot>
   </div>
 `;
 
+// this is an over-simplification for demo purposes
+// the real page loader is much more complicated
+const loadPage = function () {
+  const pageId = this._lastActiveTab[PAGE];
+  const pageTemplate = Array.from(document.querySelectorAll('template'))
+    .find(node => node.id === pageId);
+  const content = this.shadowRoot.querySelector('#content');
+  content.replaceChildren(pageTemplate.content.cloneNode(true));
+};
+
+const updateTabs = function (event) {
+  if (this._lastActiveTab) {
+    this._lastActiveTab[ACTIVE] = false;
+  }
+  this._lastActiveTab = event.target;
+
+  loadPage.call(this);
+};
+
+const watchTabs = Base => class extends Base {
+  constructor() {
+    super();
+    this._updateTabs = updateTabs.bind(this);
+    this._lastActiveTab = null;
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+
+    const tabs = this.shadowRoot.querySelector('slot').assignedNodes()
+      .filter(node => node.tagName === 'ONS-TAB');
+    const activeTab = tabs.find(tab => tab[ACTIVE]);
+    if (activeTab) {
+      this._lastActiveTab = activeTab;
+    } else {
+      this._lastActiveTab = tabs[0];
+      tabs[0][ACTIVE] = true;
+    }
+    loadPage.call(this);
+
+    this.addEventListener(`_${ACTIVE}`, this._updateTabs);
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    this.removeEventListener(`_${ACTIVE}`, this._updateTabs);
+  }
+};
+
 const tabbar = compose(
   withTemplate(template),
-  withModifier
+  withModifier,
+  watchTabs
 );
 
 export default tabbar;
