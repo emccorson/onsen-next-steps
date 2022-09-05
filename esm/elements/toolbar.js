27485ff68b8d55a5f5633e58264f2efeb11178d6
Eric Corson <public@giraffedev.com>
2022-09-05 08:22:00 +0100

    feat: add toolbar template and modifier

--- esm/elements/toolbar.js
+++ esm/elements/toolbar.js
@@ -1,7 +1,24 @@
 import { compose } from '../util.js';
+import { withModifier } from '../mixins/modifier.js';
+import { withTemplate } from '../mixins/template.js';
+
+const template = document.createElement('template');
+template.innerHTML = `
+  <div part="left">
+    <slot name="left"></slot>
+  </div>
+  <div part="center">
+    <slot name="center"></slot>
+    <slot></slot>
+  </div>
+  <div part="right">
+    <slot name="right"></slot>
+  </div>
+`;
 
 const toolbar = compose(
-
+  withTemplate(template),
+  withModifier
 );
 
 export default toolbar;
